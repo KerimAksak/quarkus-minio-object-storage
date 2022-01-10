f9baf30bcd6c5ad4c718dc103bc68a93415b1a92
KerimAksak <abdulkerim.aksak@outlook.com>
2022-01-10 16:08:44 +0300

    Read local image

---
+++ src/main/java/org/kerim/minio/resource/MinioResource.java
@@ -0,0 +1,29 @@
+package org.kerim.minio.resource;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.core.ServerResponse;
+import org.kerim.minio.service.MinioService;
+
+
+
+@Path("/minio/")
+public class MinioResource {
+
+    @Inject
+    MinioService minioService;
+    
+    @GET
+    @Path("{id}")
+    @Produces("image/jpeg")
+    public Response getImage(@PathParam("id") String id) throws Exception{
+        return Response.status(200).entity(minioService.getImage(id)).build();
+    }
+
+}
---
+++ src/main/java/org/kerim/minio/service/MinioService.java
@@ -0,0 +1,16 @@
+package org.kerim.minio.service;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import io.netty.util.internal.ResourcesUtil;
+
+@ApplicationScoped
+public class MinioService {
+    public byte[] getImage(String id) throws Exception{
+        File file = ResourcesUtil.getFile(MinioService.class, "den.png");
+        return Files.readAllBytes(file.toPath());
+    }
+}
